332fd3973a7b44e787e047a6d9f27d691e8f299b
AANGNA <antoine.alis@yahoo.fr>
2018-10-15 00:24:01 +0200

    Sensor Temperature Project

---
+++ sketch_oct14a.ino
@@ -0,0 +1,13 @@
+int pin=0;
+float tempCapteur;
+float tempFinal;
+void setup() {
+  // put your setup code here, to run once:
+seriel.begin(9600);
+pinMode(pin,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
